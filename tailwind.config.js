165bea83c7392243828245a575da26316d646e9d
Omar Lorenzo <omarlorenzo@MacBook-Pro-de-Omar.local>
2020-11-10 00:18:04 +0000

    First mobile

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    fontFamily: {
+      
+    }
+  },
+  extend: {
+    colors: {
+      danger: '#ff5f40'
+    }
+  },
+  variants: {},
+  plugins: [],
+}
